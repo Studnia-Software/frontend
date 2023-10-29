2f0dd62a372f752b85b0048b59e9a5567b3dec40
sogod <62260275+sogod7127@users.noreply.github.com>
2023-10-29 05:25:03 +0100

    Upgrade add form

---
+++ ClientFrontend/wwwroot/js/form_showing.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const showModalBtn = document.getElementById('add_button');
+    const closeModalBtn = document.getElementById('closeModalBtn');
+    const modal = document.getElementById('modal');
+
+    showModalBtn.addEventListener('click', function() {
+        modal.style.display = 'block';
+    });
+
+    closeModalBtn.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
---
+++ ClientFrontend/wwwroot/js/konto.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const anchor = document.getElementById('per_user');
+    const target = document.getElementById('add_button');
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        target.style.opacity = '0';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const anchor = document.getElementById('per_farmer');
+    const target = document.getElementById('add_button');
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        target.style.opacity = '1';
+    });
+});
